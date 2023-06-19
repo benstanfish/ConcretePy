948faf91f224c12caa48ba4ab28432a90000810e
Ben Fisher <benstanfish@hotmail.com>
2023-06-19 10:44:18 -0700

    Creation of new statics library

---
+++ statics.py
@@ -0,0 +1,8 @@
+"""Library of structural statics-related functions"""
+__author__ = "Ben Fisher"
+__version__ = "0.0.1"
+__license__ = "GPL"
+__credits__ = ["Ben Fisher"]
+__status__ = "Development"
+__maintainer__ "Ben Fisher"
+
