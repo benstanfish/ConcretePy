ca8fcfa4aa805c86fbb6d94b1f6e86762b788fba
benstanfish <benstanfish@hotmail.com>
2023-06-21 06:44:24 -0700

    .

--- statics.py
+++ statics.py
@@ -7,23 +7,23 @@ __status__ = "Development"
 
 import numpy as np
 
-_STATIONS = 101
+station_count = 101
 
 class Uniform():
     def __init__(self, w=1.0, length=1.0, E = 1.0, I = 1.0):
-        unitL = np.arange(0,1+1/_STATIONS,1/_STATIONS)
+        unitL = np.arange(0,1+1/station_count,1/station_count)
         self.length = length
         self.unitL = unitL
-        self.x = unitL * length
+        self.Xs = unitL * length
         self.w = w
         self.E = E
         self.I = I
         
         self.R1 = self.R2 = w * length / 2
-        self.V = w * (length / 2 - self.x)
-        self.M = w / 2 * self.x * (length - self.x)
+        self.V = w * (length / 2 - self.Xs)
+        self.M = w / 2 * self.Xs * (length - self.Xs)
         
-        self.d = -( w / (24 * E * I) * self.x * (length**3 - 2 * length * self.x**2 + self.x**3))
+        self.d = -( w / (24 * E * I) * self.Xs * (length**3 - 2 * length * self.Xs**2 + self.Xs**3))
         
         self.vmin = np.min(self.V)
         self.vmax = np.max(self.V)
@@ -32,15 +32,15 @@ class Uniform():
         self.dmin = np.min(self.d)
         self.dmax = np.max(self.d)
         
-        self.xvmin = self.x[np.where(self.V == np.min(self.V))[0][0]]
-        self.xvmax = self.x[np.where(self.V == np.max(self.V))[0][0]]
-        self.xmmin = self.x[np.where(self.M == np.min(self.M))[0][0]]
-        self.xmmax = self.x[np.where(self.M == np.max(self.M))[0][0]]
-        self.xdmin = self.x[np.where(self.d == np.min(self.d))[0][0]]
-        self.xdmax = self.x[np.where(self.d == np.max(self.d))[0][0]]
+        self.vmin_x = self.Xs[np.where(self.V == np.min(self.V))[0][0]]
+        self.vmax_x = self.Xs[np.where(self.V == np.max(self.V))[0][0]]
+        self.mmin_x = self.Xs[np.where(self.M == np.min(self.M))[0][0]]
+        self.mmax_x = self.Xs[np.where(self.M == np.max(self.M))[0][0]]
+        self.dmin_x = self.Xs[np.where(self.d == np.min(self.d))[0][0]]
+        self.dmax_x = self.Xs[np.where(self.d == np.max(self.d))[0][0]]
         
 
-class UniformDistr():
+class UniformDist_Pct():
     """_summary_
     """
     def __init__(self, mag = 1, length = 1, a = 0, c = 0):
@@ -51,5 +51,14 @@ class UniformDistr():
             a (int, optional): _description_. Defaults to 0.
             c (int, optional): _description_. Defaults to 0.
         """
-        self.unitL =  np.linspace(0,1,_STATIONS)
-        self.x = length * self.unitL
+        self.b = length - a - c
+        if self.b < 0:
+            self.b = 0
+        elif self.b > length:
+            self.b = length
+        
+        self.Xs = np.linspace(0,1,station_count)
+        
+        
+
+        
