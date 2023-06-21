7859d7814eebd110ab6b0b0a8dfc0dc5380d7828
benstanfish <benstanfish@hotmail.com>
2023-06-20 20:26:48 -0700

    Started new UD function

--- statics.py
+++ statics.py
@@ -7,7 +7,7 @@ __status__ = "Development"
 
 import numpy as np
 
-_STATIONS = 100
+_STATIONS = 101
 
 class Uniform():
     def __init__(self, w=1.0, length=1.0, E = 1.0, I = 1.0):
@@ -38,3 +38,18 @@ class Uniform():
         self.xmmax = self.x[np.where(self.M == np.max(self.M))[0][0]]
         self.xdmin = self.x[np.where(self.d == np.min(self.d))[0][0]]
         self.xdmax = self.x[np.where(self.d == np.max(self.d))[0][0]]
+        
+
+class UniformDistr():
+    """_summary_
+    """
+    def __init__(self, mag = 1, length = 1, a = 0, c = 0):
+        """_summary_
+
+        Args:
+            mag (int, optional): _description_. Defaults to 1.
+            a (int, optional): _description_. Defaults to 0.
+            c (int, optional): _description_. Defaults to 0.
+        """
+        self.unitL =  np.linspace(0,1,_STATIONS)
+        self.x = length * self.unitL
