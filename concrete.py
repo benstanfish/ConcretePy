babc18b56b0393908f7ace93025d3ea18824e14e
Ben Fisher <benstanfish@hotmail.com>
2023-07-19 16:24:04 -0700

    Updates

--- concrete.py
+++ concrete.py
@@ -1,14 +1,13 @@
 import materials
 from math import sqrt, copysign, abs
 
-def get_a(bw, Astl, concrete: materials.ConcreteMaterial,
-          steel: materials.SteelMaterial):
+def get_a(bw, Astl, concrete: materials.ConcreteMaterial, steel: materials.SteelMaterial):
     try:
         return steel.fy * Astl / (0.85 * concrete.fc * bw)
     except ZeroDivisionError:
         return 0
 
-def get_c(a, concrete: materials.ConcreteMaterial):
+def get_c_from_a(a, concrete: materials.ConcreteMaterial):
     try:
         return a / concrete.b1
     except ZeroDivisionError:
@@ -20,13 +19,13 @@ def get_c_from_Z(z, d, concrete: materials.ConcreteMaterial, steel: materials.St
     except ZeroDivisionError:
         return 0
 
-def get_c_from_d(es, d, concrete: materials.ConcreteMaterial):
+def get_c_from_d(strain:float , d:float , concrete: materials.ConcreteMaterial):
     try:
-        return d / (1 - es / concrete.ecu)
+        return d / (1 - strain / concrete.ecu)
     except ZeroDivisionError:
         return 0
 
-def get_es(c,d,concrete: materials.ConcreteMaterial):
+def get_es(c, d, concrete: materials.ConcreteMaterial):
     try:
         return concrete.ecu*(1-d/c)
     except ZeroDivisionError:
@@ -38,43 +37,5 @@ def get_fs(strain: float, steel: materials.SteelMaterial):
     else:
         return copysign(steel.fy,strain)
 
-def get_Vc(bw, d, concrete: materials.ConcreteMaterial):
-    return 2 * concrete.lam * sqrt(concrete.fc) * bw * d
-
-def get_Vc_with_axial(bw, d, Nu, Ag, concrete: materials.ConcreteMaterial,
-                      isSignificantTension: bool = False):
-    # Eq. (22.5.6.1) or (22.5.7.1)
-    denominator = 2000
-    if isSignificantTension == True:
-        denominator = 500
-    return max((1 + Nu/(denominator * Ag)) * get_Vc(bw, d, concrete),0)
-
-def get_Av_s(Vu, Vc, d, steel: materials.SteelMaterial,
-             isSeisShear: bool = False):
-    try:
-        phiV = 0.75
-        if isSeisShear == True:
-            phiV = 0.6
-        return (Vu - phiV * Vc) / (phiV * steel.fy * d)
-    except ZeroDivisionError:
-        return 0
-
-# Shear wall methods
-def get_ac(hw: float, lw: float):
-    # Alpha_c per ACI 318 Sec. 18.10.4.1
-    try:
-        if hw/lw <= 1.5:
-            return 3
-        elif hw/lw >= 2.0:
-            return 2
-        else:
-            return 3 - 2 * (hw/lw - 1.5)
-    except ZeroDivisionError:
-        return 0
-
-
-
-
-
-
-# PM Methods
+def geometric_sequence(n, initial, common_ratio):
+    return initial * common_ratio ^ (n - 1)
