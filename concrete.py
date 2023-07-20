a83d9422da76774e816b620d886f6ecfdfb01732
Ben Fisher <benstanfish@hotmail.com>
2023-07-20 11:44:57 -0700

    refactored and added coments/docstrings

--- concrete.py
+++ concrete.py
@@ -1,5 +1,9 @@
+"""Library of functions to create PM coordinates for creating concrete PM diagrams"""
+
+import matplotlib.pyplot as plt
 import numpy as np
-import materials
+
+import materials as mat
 
 def geometric_sequence(n, initial, common_ratio):
     return initial * common_ratio ^ (n - 1)
@@ -7,16 +11,16 @@ def geometric_sequence(n, initial, common_ratio):
 def layerDistances(n, db, cc, h):
     return np.linspace(cc + db/2, h-cc-db/2, n)
 
-def maximumCompression(Ag, layer_areas, concrete: materials.ConcreteMaterial, rebar: materials.RebarMaterial):
+def maximumCompression(Ag, layer_areas, concrete: mat.ConcreteMaterial, rebar: mat.RebarMaterial):
     return (0.85 * concrete.fc) * (Ag - np.sum(layer_areas)) + rebar.fy * np.sum(layer_areas)
 
-def cFromZ(Z, d, concrete: materials.ConcreteMaterial, rebar: materials.RebarMaterial):
+def cFromZ(Z, d, concrete: mat.ConcreteMaterial, rebar: mat.RebarMaterial):
     try:
         return concrete.ecu/(concrete.ecu - Z * rebar.ey)*d
     except ZeroDivisionError:
         return 0
     
-def zFromC(c, d, concrete: materials.ConcreteMaterial, rebar: materials.RebarMaterial):
+def zFromC(c, d, concrete: mat.ConcreteMaterial, rebar: mat.RebarMaterial):
     if c != 0:
         try:
             return concrete.ecu / rebar.ey * (1 - d / abs(c))
@@ -26,36 +30,36 @@ def zFromC(c, d, concrete: materials.ConcreteMaterial, rebar: materials.RebarMat
         # c = 0 is error case; pass 250 which is well past the rupture strain
         return 250
 
-def layerStrain(layer_distance, c, concrete: materials.ConcreteMaterial):
+def layerStrain(layer_distance, c, concrete: mat.ConcreteMaterial):
     try:
         return (c - layer_distance)/c * concrete.ecu
     except:
         return 0
     
-def layerStress(layer_distance, c, concrete: materials.ConcreteMaterial, rebar: materials.RebarMaterial):
+def layerStress(layer_distance, c, concrete: mat.ConcreteMaterial, rebar: mat.RebarMaterial):
     strain = layerStrain(layer_distance, c, concrete)
     return min(rebar.fy * np.sign(strain), strain * rebar.Es)
 
-def layerForce(layer_area, layer_distance, c, concrete: materials.ConcreteMaterial, rebar: materials.RebarMaterial):
+def layerForce(layer_area, layer_distance, c, concrete: mat.ConcreteMaterial, rebar: mat.RebarMaterial):
     if layer_distance > c:
         return layerStress(layer_distance, c, concrete, rebar) * layer_area
     else:
         return (layerStress(layer_distance, c, concrete, rebar) - 0.85 * concrete.fc) * layer_area
     
-def sumLayerForces(layer_areas, layer_distances, c, concrete: materials.ConcreteMaterial, rebar: materials.RebarMaterial):
+def sumLayerForces(layer_areas, layer_distances, c, concrete: mat.ConcreteMaterial, rebar: mat.RebarMaterial):
     force = 0
     for i in range(layer_areas.shape[0]):
         force += layerForce(layer_areas[i], layer_distances[i], c, concrete, rebar)
     return force
 
     
-def sumLayerMoments(layer_areas, layer_distances, h, c, concrete: materials.ConcreteMaterial, rebar: materials.RebarMaterial):
+def sumLayerMoments(layer_areas, layer_distances, h, c, concrete: mat.ConcreteMaterial, rebar: mat.RebarMaterial):
     moment = 0
     for i in range(layer_areas.shape[0]):
         moment += layerForce(layer_areas[i], layer_distances[i], c, concrete, rebar) * (h/2 - layer_distances[i])
     return moment
 
-def PMPoints(c, bw, h, layer_distances, layer_areas, concrete: materials.ConcreteMaterial, rebar: materials.RebarMaterial):
+def PMPoints(c, bw, h, layer_distances, layer_areas, concrete: mat.ConcreteMaterial, rebar: mat.RebarMaterial):
     sum_Fs = 0
     sum_Ms = 0
     for i in range(layer_areas.shape[0]):
@@ -67,14 +71,9 @@ def PMPoints(c, bw, h, layer_distances, layer_areas, concrete: materials.Concret
     M = Mc + sum_Ms
     return P, M
 
-def ZfromP(Za, Zb, PDiff, bw, h, layer_distances, layer_areas, concrete: materials.ConcreteMaterial, rebar: materials.RebarMaterial):
-    """Return Z for a given P value (PDiff), given two bounding Z values (Za and Zb)
-
-    Args:
-        Za (_type_): lower bound Z
-        Zb (_type_): upper bound Z
-        PDiff (_type_): axial demand (compression is positive)
-    """
+def ZfromP(Za, Zb, P, bw, h, layer_distances, layer_areas, concrete: mat.ConcreteMaterial, rebar: mat.RebarMaterial):
+    """Find Z (relating to ey) for a given P value. Solve using the Newton-Rapfson root finding method.
+    Calculated Pi values are shifted down by P (such that P => y = 0) to solve for it as a root."""
     
     minTol = 0.0000000001
     maxIter = 100
@@ -86,12 +85,12 @@ def ZfromP(Za, Zb, PDiff, bw, h, layer_distances, layer_areas, concrete: materia
     while keepRunning == True:
         ca = cFromZ(Za, d, concrete, rebar)
         #cb = cFromZ(Zb, d, concrete, rebar)
-        Pa = PMPoints(ca, bw, h, layer_distances, layer_areas, concrete, rebar)[0] - PDiff
-        #Pb = PMPoints(cb, bw, h, layer_distances, layer_areas, concrete, rebar)[0] - PDiff
+        Pa = PMPoints(ca, bw, h, layer_distances, layer_areas, concrete, rebar)[0] - P
+        #Pb = PMPoints(cb, bw, h, layer_distances, layer_areas, concrete, rebar)[0] - P
 
         Zc = (Za + Zb) / 2
         cc = cFromZ(Zc, d, concrete, rebar)
-        Pc = PMPoints(cc, bw, h, layer_distances, layer_areas, concrete, rebar)[0] - PDiff 
+        Pc = PMPoints(cc, bw, h, layer_distances, layer_areas, concrete, rebar)[0] - P 
 
         if abs(Pc) == 0:
             return Zc
--- materials.py
+++ materials.py
@@ -1,10 +1,10 @@
+"""Library of material-related functions, based on ACI 318 and US customary units (inches, pounds)"""
+
 from math import sqrt
-from typing import List, Union, Any, Dict
 
-# default properties for US materials using ACI 318 and imperial units
-
-# reinforcement values per ACI 318, Appendix A
+# Rebar sizes, diameters, areas, weights per ACI 318 Appendix A
 bar_numbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 14, 18]
+
 bar_diameters = {
     3: 0.375,
     4: 0.500,
@@ -18,6 +18,7 @@ bar_diameters = {
     14: 1.693,
     18: 2.257
 }
+
 bar_areas = {
     3: 0.11,
     4: 0.20,
@@ -31,6 +32,7 @@ bar_areas = {
     14: 2.25,
     18: 4.00
 }
+
 bar_weights = {
     3: 0.376,
     4: 0.668,
@@ -45,27 +47,73 @@ bar_weights = {
     18: 13.6
 }
 
+# Material classes
 class ConcreteMaterial:
     def __init__(self, fc, lam=1.0):
-        self.fc = fc  # Min 28-day strength (psi)
-        self.lam = lam  # LWC factor lambda
-        self.ecu = 0.003  # Crushing strain
-        self.Ec = 57000 * sqrt(self.fc)  # Elastic modulus (psi)
-        self.fr = 7.5 * lam * sqrt(fc)  # Modulus of rupture (psi)
-        self.b1 = self.beta1
+        self.fc = fc  # Minimum specified 28-day compressive strength (psi), user-specified.
+        self.lam = lam  # Lambda factor for light-weight concrete (LWC) per ACI 318 Table 19.2.4.2.
+        self.ecu = 0.003  # Maximum concrete compression strain per ACI 318 Sec. 22.2.2.1.
+        self.Ec = 57000 * sqrt(self.fc)  # Elastic modulus (psi) per ACI 318 Eq. (19.2.2.1.b).
+        self.fr = 7.5 * lam * sqrt(fc)  # Modulus of rupture (psi) per ACI 318 Eq. (19.2.3.1).
+        self.b1 = self.beta1  # beta1 factor relating the "a" and "c" distances per ACI 318 Sec. 22.2.2.4.1.
 
     @property
     def beta1(self):
+        """Calculate the beta1 factor per ACI 318 Eq. (22.2.2.4.1) and Table 22.2.2.4.3"""
         if self.fc <= 4000:
-            return 0.85
+            return 0.85  # ACI 318 Table 22.2.2.4.3 case (a)
         elif self.fc >= 8000:
-            return 0.65
+            return 0.65  # ACI 318 Table 22.2.2.4.3 case (c)
         else:
-            return 0.85 - 0.05 * (self.fc - 4000) / 1000
+            return 0.85 - 0.05 * (self.fc - 4000) / 1000  # ACI 318 Table 22.2.2.4.3 case (b)
 
-
-class RebarMaterial:
+class SteelMaterial:
     def __init__(self, fy = 60000):
-        self.fy = fy  # Mim steel yield stress (psi)
-        self.Es = 29000000  # Elastic modulus (psi)
-        self.ey = self.fy / self.Es  # Steel yield strain
+        self.fy = fy  # Minimum specified yeild-strength of rebar (psi), user-specified.
+        self.Es = 29000000  # Modulus of elasticity for nonprestressed bars and wires per ACI 318 Sec. 20.2.2.2
+        self.ey = self.fy / self.Es  # Yeild strain of the rebar, refer to ACI 318 Sec. R20.2.2.1
+        
+class RebarMaterial(SteelMaterial):
+    def __init__(self, fy=60000):
+        super().__init__(fy)
+        self.bar_numbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 14, 18]
+        self.bar_diameters = {
+                            3: 0.375,
+                            4: 0.500,
+                            5: 0.625,
+                            6: 0.750,
+                            7: 0.875,
+                            8: 1.000,
+                            9: 1.128,
+                            10: 1.270,
+                            11: 1.410,
+                            14: 1.693,
+                            18: 2.257
+                            }   
+        self.bar_areas = {
+                            3: 0.11,
+                            4: 0.20,
+                            5: 0.31,
+                            6: 0.44,
+                            7: 0.60,
+                            8: 0.79,
+                            9: 1.00,
+                            10: 1.27,
+                            11: 1.56,
+                            14: 2.25,
+                            18: 4.00
+                        }
+        self.bar_weights = {
+                            3: 0.376,
+                            4: 0.668,
+                            5: 1.043,
+                            6: 1.503,
+                            7: 2.044,
+                            8: 2.67,
+                            9: 3.4,
+                            10: 4.303,
+                            11: 5.313,
+                            14: 7.65,
+                            18: 13.6
+                        }
+        
